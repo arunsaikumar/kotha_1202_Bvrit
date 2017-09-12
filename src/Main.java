095d9ffd5acb6d51a07da9857b316469289dde5e
arunsaikumar <31847155+arunsaikumar@users.noreply.github.com>
2017-09-12 13:31:19 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student s = new Student(1202,"Arun",null,93.05);
+		s.setId(1202);
+		s.setFullName("Arun Sai kumar");
+		//s.setBirthDate("1996-11-21");
+		s.setAvgMark(95.35);
+		
+		
+		
+		
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
